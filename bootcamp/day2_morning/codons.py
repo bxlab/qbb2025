bfa4732e0af87e6364b6ab8148792b2b215f17ea
msauria <mike.sauria@gmail.com>
2025-09-02 09:55:36 -0400

    Added day2 livecoding

---
+++ bootcamp/day2_morning/codons.py
@@ -0,0 +1,66 @@
+codontab = {
+    'UCA': 'S',    # Serina
+    'UCC': 'S',    # Serina
+    'UCG': 'S',    # Serina
+    'UCU': 'S',    # Serina
+    'UUC': 'F',    # Fenilalanina
+    'UUU': 'F',    # Fenilalanina
+    'UUA': 'L',    # Leucina
+    'UUG': 'L',    # Leucina
+    'UAC': 'Y',    # Uirosina
+    'UAU': 'Y',    # Uirosina
+    'UAA': '*',    # Stop
+    'UAG': '*',    # Stop
+    'UGC': 'C',    # Cisteina
+    'UGU': 'C',    # Cisteina
+    'UGA': '*',    # Stop
+    'UGG': 'W',    # Uriptofano
+    'CUA': 'L',    # Leucina
+    'CUC': 'L',    # Leucina
+    'CUG': 'L',    # Leucina
+    'CUU': 'L',    # Leucina
+    'CCA': 'P',    # Prolina
+    'CCC': 'P',    # Prolina
+    'CCG': 'P',    # Prolina
+    'CCU': 'P',    # Prolina
+    'CAC': 'H',    # Histidina
+    'CAU': 'H',    # Histidina
+    'CAA': 'Q',    # Glutamina
+    'CAG': 'Q',    # Glutamina
+    'CGA': 'R',    # Arginina
+    'CGC': 'R',    # Arginina
+    'CGG': 'R',    # Arginina
+    'CGU': 'R',    # Arginina
+    'AUA': 'I',    # Isoleucina
+    'AUC': 'I',    # Isoleucina
+    'AUU': 'I',    # Isoleucina
+    'AUG': 'M',    # Methionina
+    'ACA': 'T',    # Treonina
+    'ACC': 'T',    # Treonina
+    'ACG': 'T',    # Treonina
+    'ACU': 'T',    # Treonina
+    'AAC': 'N',    # Asparagina
+    'AAU': 'N',    # Asparagina
+    'AAA': 'K',    # Lisina
+    'AAG': 'K',    # Lisina
+    'AGC': 'S',    # Serina
+    'AGU': 'S',    # Serina
+    'AGA': 'R',    # Arginina
+    'AGG': 'R',    # Arginina
+    'GUA': 'V',    # Valina
+    'GUC': 'V',    # Valina
+    'GUG': 'V',    # Valina
+    'GUU': 'V',    # Valina
+    'GCA': 'A',    # Alanina
+    'GCC': 'A',    # Alanina
+    'GCG': 'A',    # Alanina
+    'GCU': 'A',    # Alanina
+    'GAC': 'D',    # Acido Aspartico
+    'GAU': 'D',    # Acido Aspartico
+    'GAA': 'E',    # Acido Glutamico
+    'GAG': 'E',    # Acido Glutamico
+    'GGA': 'G',    # Glicina
+    'GGC': 'G',    # Glicina
+    'GGG': 'G',    # Glicina
+    'GGU': 'G'     # Glicina
+}
